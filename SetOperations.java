b4b478ca08f149af83872770e3f2deca833e01cb
lai8a <138739327+lai8a@users.noreply.github.com>
2023-10-27 19:10:20 -0400

    Add files via upload

--- SetOperations.java
+++ SetOperations.java
@@ -1,14 +1,133 @@
+
+/**
+* Methods for Set Operations and formulas covered in class. 
+* 
+* 
+* <p>
+* These methods can be called within the terminal, using Method (parameters)
+* 
+* 
+* 
+* Mean Median Mode:
+* @param  listofnums  an absolute URL giving the base location of the image
+* @return 
+* 
+*/
 //--------------------------------------------------------------------
 // Laiba Khan, CSCI3327 Probability and Applied Statistics
-// Chapter 2:
-// StatsLibrary: SetOperations (Union, Intersect, Compliment)
 //--------------------------------------------------------------------
+
+import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 
 public class SetOperations {
+
+//Chapter 1: Mean Median Mode ----------------------------------------
+
+    public static Double Mean(double[] listofnums){
+        //Return a 0 if there is no numbers within the list. 
+        //This way, you can avoid doing unnecessary math.
+        if (listofnums.length == 0){
+            return Double.NaN;
+        }
+        else{
+            double numerator = 0.0;
+            //Iterates through list and adds each value to 
+            //create the numerator for the function.
+            for (double value : listofnums){
+                numerator += value;
+            }
+            //Sum of all values divided by number of terms.
+            return numerator/listofnums.length;            
+        }
+    }
+    
+    public static Double Median(double[] listofnums){
+        if (listofnums.length == 0){
+            return Double.NaN;
+        }
+        //Calculates the index for value in the middle of the array.
+        //If the index is even, it takes the mean of the two middle numbers.
+        //If not, the value of the index is simply returned.
+        int medianindex = listofnums.length/2;
+        if (listofnums.length % 2 == 0){
+            double median = (listofnums[medianindex] + listofnums[medianindex - 1]) / 2;
+            return median;
+        }
+        else {
+            double median = listofnums[medianindex];
+            return median;
+        }
+    }
+
+    public static List<Double> Mode(double[] listofnums){
+        //Create a new list to track the occurences of each value.
+        List<Double> Mode = new ArrayList<>();
+        int maxOccurrences = 0;
+        
+        //Return alternate values or a string if there is 0 or 1 values in list.
+        //This way, you avoid any unecessary math.
+        if (listofnums.length == 0){
+            Mode.add(Double.NaN);
+        }
+        else if (listofnums.length == 1){
+            Mode.add(listofnums[0]);
+        }
+
+        else{
+            for (double value : listofnums){
+                int occurrences = 0;
+            
+                for (double num : listofnums){
+                    if (value == num){
+                        occurrences++;
+                    }
+                }
+
+                if (occurrences > maxOccurrences){
+                    maxOccurrences = occurrences;
+                    Mode.clear();
+                    Mode.add(value);
+                }
+                else if (occurrences == maxOccurrences && !Mode.contains(value)){
+                    Mode.add(value);
+                }
+            }    
+        }
+    
+    return Mode;  
+    } 
+
+//Chapter 1: Standard Deviation / Variance ----------------------------------------
+
+    public static Double StandDev(double[] listofnums){
+        double N, mean, SD;
+        double sum = 0.0;
+        //Find mean.
+        mean = Mean(listofnums);
+        N = listofnums.length;
+        //Find sum(x - mean)^2 portion of equation.
+        for (double x : listofnums){
+            double distsquare = (x - mean) * (x - mean);
+            sum += distsquare;
+        }
+
+        SD = Math.sqrt(sum / N);
+        return SD;
+    }
+
+    //Variance is Standard Deviation to the second power. V = 
+    public static Double Variance(double SD){
+        //Input result from the Standard Devition method and get variance.
+        double variance = SD * SD;
+        return variance;
+    }
+
+//Chapter 2: Union Intersect Compliment ----------------------------------------
+
     //Union is the combinations of two lists.
     //Take the parameters of two lists.
     public static List<Integer> Union(Integer[] list1, Integer[] list2) {
@@ -58,19 +177,101 @@ public class SetOperations {
         return compliment;
     }
 
-    public static void main(String[] tests){
-        Integer l1test1[] = {1, 2, 3, 4, 5, 6, 7};
-        Integer l2test1[] = {5, 6, 7, 8, 9, 10, 11};
-        Integer A[] = {1, 2, 3, 4, 5};
-        // Print Union test.
-        System.out.println("Union of " + Arrays.toString(l1test1) +
-        " and " + Arrays.toString(l2test1) + " is " + Union(l1test1, l2test1));
-        // Print Intersect test.
-        System.out.println("Intersect of " + Arrays.toString(l1test1) +
-        " and " + Arrays.toString(l2test1) + " is " + Intersect(l1test1, l2test1));
-        // Print Compliment test.
-        System.out.println("Compliment of " + Arrays.toString(l1test1) +
-        " and subset " + Arrays.toString(A) + " is " + Compliment(l1test1, A));
+//Chapter 2: Factorial (Big Integer) ---------------------------------------- 
+
+    public static BigInteger Factorial(int n){
+
+        //If input is 0, then just return 0.
+        if (n == 0){
+            //Researched this on https://docs.oracle.com/javase/8/docs/api/java/math/BigInteger.html.
+            //BigInteger.ONE is the BigInteger 1 constant.
+            //Output a 1 because 0! = 1.
+            return BigInteger.ONE;
+        }
+
+        //Initialize result as 1. You also want to start loop index at 0 
+        //because of this.
+        BigInteger result = BigInteger.ONE;
+
+        //Then loop from 1 to n, and multiply each value by the result.
+        for (int i = 1; i <= n; i++) {
+            result = result.multiply(BigInteger.valueOf(i));
+        }
+        return result;
+    }
+    
+//Chapter 2: Permutations Combinations ---------------------------------------- 
+
+    public static BigInteger Permutations(int n, int r){
+        //Solve for nPr = n!/(n-r)!.
+        //n is the total number of objects.
+        //r is the number of objects selected.
+
+        //Only run if n is greater or equal to r, since it would not be possible
+        //to do a permutation otherwise.
+        if (n >= r){
+            BigInteger numerator = Factorial(n);
+            int temp = n - r;
+            BigInteger denominator = Factorial(temp);
+            BigInteger result = numerator.divide(denominator);
+            return result;            
+        }
+        else {
+            throw new RuntimeException("\nERROR: you selected more objects than you have");
+        }
+ 
+    }
+
+    public static BigInteger Combinations(int n, int r){
+        //Solve for nCr = n!/r!(n-r)!.
+        //Just multiply Permutation equation by 1/r!.
+
+        //Initialize value 1 and r! to variables
+        BigInteger rFact = Factorial(r);
+        BigInteger one = BigInteger.ONE;
+
+        //Find 1/r!.
+        BigInteger fraction = one.divide(rFact);
+
+        //Find permutation.
+        BigInteger perm = Permutations(n, r);
+
+        //Multiply them.
+        BigInteger result = fraction.multiply(perm);
+
+        return result;
+
+    }
+
+    public static Double Conditional(Integer[] A, Integer[] B, Integer[] s){
+        //Solve for P(A|B) = P(A ∩ B)/P(B).
+        //Probability of A given B has occured.
+        //'∩' is symbol for intersection.
+        
+        //Keep in mind P(A ⋂ B) = n(A ⋂ B)/n(S), where n(A ⋂ B) is number of elements in set A and B
+        //and n(S) is number is elements in the sample space
+        
+        List<Integer> AinterB = Intersect(A, B);
+
+        Integer numAinterB = AinterB.size();
+
+        //n(S)
+        Integer slength = s.length;
+        //Length of A and B.
+        Integer Alength = A.length;
+        Integer Blength =  B.length;
+
+
+        double PA = (double) Alength / slength;
+        double PB = (double) Blength / slength;
+
+        //P(A ⋂ B) = n(A ⋂ B)/n(S)
+        Integer probAinterB = numAinterB / numAinterB;
+
+        double result = probAinterB / PB;
+
+        return result;
+ 
     }
 }
 
